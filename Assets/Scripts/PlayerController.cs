55c56db319765e1e421cca1de23354038b572011
Yury Trypolski <yury1011.89@gmail.com>
2023-11-03 19:56:03 +0300

    add added the functionality of pushing the enemy away when capturing a star.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private GameObject focusPoint;
     private float speedPlayer = 15.0f;
     private Rigidbody playerRB;
+    public bool hasPowerUp;
+    private float powerUpStrenght = 10.0f;
     void Start() {
         playerRB = GetComponent<Rigidbody>();
         focusPoint = GameObject.Find("Focal Point");
@@ -14,5 +18,26 @@ public class PlayerController : MonoBehaviour {
     void Update() {
         float verticalInput = Input.GetAxis("Vertical");
         playerRB.AddForce(speedPlayer * verticalInput * focusPoint.transform.forward);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Powerup")) {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Enemy") && hasPowerUp) {
+            Rigidbody enemyRB = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.transform.position - transform.position;
+            enemyRB.AddForce(awayFromPlayer * powerUpStrenght, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine () {
+        yield return new WaitForSeconds(5);
+        hasPowerUp = false;
     }
 }
