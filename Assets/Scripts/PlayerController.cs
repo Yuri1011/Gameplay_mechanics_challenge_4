7a51b14bc4af83015eca2bad6622c48007f0877a
Yury Trypolski <yury1011.89@gmail.com>
2023-11-03 12:55:49 +0300

    the player moves in the direction of the camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    private GameObject focusPoint;
     private float speedPlayer = 15.0f;
     private Rigidbody playerRB;
     void Start() {
         playerRB = GetComponent<Rigidbody>();
+        focusPoint = GameObject.Find("Focal Point");
     }
 
     void Update() {
         float verticalInput = Input.GetAxis("Vertical");
-        playerRB.AddForce(speedPlayer * verticalInput * Vector3.forward);
+        playerRB.AddForce(speedPlayer * verticalInput * focusPoint.transform.forward);
     }
 }
