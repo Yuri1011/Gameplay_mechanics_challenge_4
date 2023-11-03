d7ff9d6eadd6aba43bbae35fb4fae9778ca95bcc
Yury Trypolski <yury1011.89@gmail.com>
2023-11-03 13:28:55 +0300

    add function for generate random position enemy

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,12 +6,15 @@ public class SpawnManager : MonoBehaviour {
     public GameObject spawnEnemyPrefab;
     private float spawnRange = 9.0f;
     void Start() {
+        Instantiate(spawnEnemyPrefab, GenerateRandomSpawnPosition(), spawnEnemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateRandomSpawnPosition() {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-        Instantiate(spawnEnemyPrefab, randomPos, spawnEnemyPrefab.transform.rotation);
+        return randomPos;
     }
-
     void Update() {
         
     }
