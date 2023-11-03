50366f815eecad4a3ae0023ab3d7d44f76a633ab
Yury Trypolski <yury1011.89@gmail.com>
2023-11-03 12:22:35 +0300

    Challenge_4 with balls

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+    private float speedRotate = 20.0f;
+
+    void Start() {
+    }
+
+    void Update() {
+        float horizontalInput = -Input.GetAxis("Horizontal");
+        transform.Rotate(speedRotate * horizontalInput * Time.deltaTime * Vector3.up);
+    }
+}
